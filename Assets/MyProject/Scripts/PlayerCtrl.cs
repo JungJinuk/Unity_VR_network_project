3424465496f113f05e130f07c7eb6df33705f18c
jinuk4217 <jinwook4217@daum.net>
2018-12-02 00:49:32 +0900

    first commit

---
+++ Assets/MyProject/Scripts/PlayerCtrl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerCtrl : NetworkBehaviour {
+
+	public Transform MainCamTrans {
+		get {
+			if (cashedMainCamTrnas == null) {
+				cashedMainCamTrnas = Camera.main.transform;
+			}
+			return cashedMainCamTrnas;
+		}
+	}
+	private Transform cashedMainCamTrnas;
+
+	public Transform MainCamOffset {
+		get {
+			if (cashedMainCamOffset == null) {
+				cashedMainCamOffset = Camera.main.transform.parent;
+			}
+			return cashedMainCamOffset;
+		}
+	}
+	private Transform cashedMainCamOffset;
+
+	private Vector3 spawnPos;
+
+	void Start() {
+		if (!isLocalPlayer) {
+			Destroy(this);
+			return;
+		}
+		spawnPos = NetworkManager.singleton.GetStartPosition().position;
+	}
+
+	void Update () {
+		transform.position = MainCamTrans.localPosition + spawnPos;
+		transform.rotation = MainCamTrans.localRotation;
+
+		MainCamOffset.position = transform.position;
+	}
+}
---
+++ Assets/MyProject/Scripts/VRManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VRManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SwitchToVRMode());
+	}
+
+	IEnumerator SwitchToVRMode() {
+		VRSettings.LoadDeviceByName("cardboard");
+		yield return null;
+		VRSettings.enabled = true;
+	}
+}
